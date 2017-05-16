093f4a1c8c01d965fec83a1bc7487ef63ef0b28a
Robin Heinze <robin.m.heinze@gmail.com>
2017-05-16 15:00:48 -0700

    and again

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -168,7 +168,7 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         if (activity == null) return;
         try
         {
-            FLog.d("calling onHostDestroy RMH");
+            FLog.d(ReactConstants.TAG, "calling onHostDestroy RMH");
         }
         catch (java.lang.IllegalArgumentException e) {
             FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
